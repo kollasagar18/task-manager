aaed1b04ed816de6422b636924a8d811931e3a1d
kollasagar18 <kollasagar03@gmail.com>
2026-05-10 16:15:24 +0530

    fixed syntax error in db.py

--- not/db.py
+++ not/db.py
@@ -7,6 +7,6 @@ def db_con():
         host=os.getenv("MYSQLHOST", "localhost"),
         user=os.getenv("MYSQLUSER", "root"),
         password=os.getenv("MYSQLPASSWORD", "Kollasagar@93"),
-        database=os.getenv("MYSQLDATABASE", "railway"),
+        database=os.getenv("MYSQLDATABASE", "primetrade"),
         port=os.getenv("MYSQLPORT", "3306")
     )
